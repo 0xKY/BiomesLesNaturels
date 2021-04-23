b97ac91c00e4aa8871a578d19fcc8223bc5fb4a1
KaloyanKKS <k.yunchov9@gmail.com>
2021-04-23 21:12:12 +0100

    Cleanup for first release

--- src/main/java/me/kaloyankys/biomeslesnaturels/modinit/ModBlocks.java
+++ src/main/java/me/kaloyankys/biomeslesnaturels/modinit/ModBlocks.java
@@ -3,31 +3,35 @@ package me.kaloyankys.biomeslesnaturels.modinit;
 import me.kaloyankys.biomeslesnaturels.block.*;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.GrassBlock;
 import net.minecraft.block.Material;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
 
-    public static final Block OAT_GRASS = new OatGrassBlock(FabricBlockSettings.of(Material.PLANT).strength(0f).collidable(false));
-    public static final Block OAT_GRASS_SMALL = new OatGrassSmall(FabricBlockSettings.of(Material.PLANT).strength(0f).collidable(false));
-    public static final Block LIMESTONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5f));
-    public static final Block LAYERED_LIMESTONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5f));
-    public static final Block SUNLEAF = new SunLeafBlock(FabricBlockSettings.of(Material.PLANT).strength(0f).collidable(false));
-    public static final Block OAT_BUNDLE = new Block(FabricBlockSettings.of(Material.PLANT).strength(0.5f));
-    public static final Block GEYSER = new Geyser(FabricBlockSettings.of(Material.PLANT).strength(1.7f));
-    public static final Block RUFFROCK = new Block(FabricBlockSettings.of(Material.PLANT).strength(1.7f));
+    public static final Block OAT_GRASS = new OatGrassBlock(FabricBlockSettings.of(Material.PLANT).breakInstantly().noCollision());
+    public static final Block OAT_GRASS_SMALL = new OatGrassSmall(FabricBlockSettings.of(Material.PLANT).breakInstantly().noCollision());
+    public static final Block LIMESTONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5f).breakByTool(FabricToolTags.PICKAXES, 1));
+    public static final Block LAYERED_LIMESTONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5f).breakByTool(FabricToolTags.PICKAXES, 1));
+    public static final Block SMOOTH_LIMESTONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5f).breakByTool(FabricToolTags.PICKAXES, 1));
+    public static final Block SMOOTH_LIMESTONE_BRICKS = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5f).breakByTool(FabricToolTags.PICKAXES, 1));
+    public static final Block SUNLEAF = new SunLeafBlock(FabricBlockSettings.of(Material.PLANT).breakInstantly().noCollision());
+    public static final Block OAT_BUNDLE = new Block(FabricBlockSettings.of(Material.PLANT).strength(0.5f).breakByTool(FabricToolTags.HOES, 1));
+    public static final Block GEYSER = new Geyser(FabricBlockSettings.of(Material.PLANT).strength(1.7f).breakByTool(FabricToolTags.PICKAXES, 1));
+    public static final Block RUFFROCK = new Block(FabricBlockSettings.of(Material.PLANT).strength(1.7f).breakByTool(FabricToolTags.PICKAXES, 1));
+    public static final Block POLISHED_RUFFROCK = new Block(FabricBlockSettings.of(Material.PLANT).strength(1.7f).breakByTool(FabricToolTags.PICKAXES, 1));
+    public static final Block POLISHED_RUFFROCK_TILES = new Block(FabricBlockSettings.of(Material.PLANT).strength(1.7f).breakByTool(FabricToolTags.PICKAXES, 1));
     public static final Block GOATBERRY = new GoatberryBush(FabricBlockSettings.of(Material.PLANT).strength(0.5f));
-    public static final Block MINT = new AbstractGrass(FabricBlockSettings.of(Material.PLANT).strength(0f).collidable(false));
-    public static final Block WIND = new WindBlock(FabricBlockSettings.of(Material.AIR).strength(0f).collidable(false));
-    public static final Block FLOURISHING_WINDGRASS = new AbstractGrass(FabricBlockSettings.of(Material.PLANT).strength(0f).collidable(false));
+    public static final Block MINT = new AbstractGrass(FabricBlockSettings.of(Material.PLANT).strength(0f).noCollision());
+    public static final Block WIND = new WindBlock(FabricBlockSettings.of(Material.AIR).strength(1000f).noCollision());
+    public static final Block FLOURISHING_WINDGRASS = new AbstractGrass(FabricBlockSettings.of(Material.PLANT).breakInstantly().noCollision());
     public static final Block MUDDY_DIRT = new GrassBlock(FabricBlockSettings.of(Material.SOIL).strength(0.5f));
-    public static final Block EBONY_TREE = new EbonyTree(FabricBlockSettings.of(Material.WOOD).strength(0.9f).collidable(false));
-    public static final Block EDELWEISS = new AbstractGrass(FabricBlockSettings.of(Material.WOOD).strength(0.9f).collidable(false));
+    public static final Block EBONY_TREE = new EbonyTree(FabricBlockSettings.of(Material.WOOD).strength(0.9f).noCollision().breakByTool(FabricToolTags.AXES, 1));
+    public static final Block EDELWEISS = new AbstractGrass(FabricBlockSettings.of(Material.PLANT).breakInstantly().noCollision());
 
     public ModBlocks() {
 
@@ -61,6 +65,13 @@ public class ModBlocks {
         Registry.register(Registry.ITEM, new Identifier("biomeslesnaturels", "ebony_tree"), new BlockItem(EBONY_TREE, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
         Registry.register(Registry.BLOCK, new Identifier("biomeslesnaturels", "edelweiss"), EDELWEISS);
         Registry.register(Registry.ITEM, new Identifier("biomeslesnaturels", "edelweiss"), new BlockItem(EDELWEISS, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
-
+        Registry.register(Registry.BLOCK, new Identifier("biomeslesnaturels", "smooth_limestone"), SMOOTH_LIMESTONE);
+        Registry.register(Registry.ITEM, new Identifier("biomeslesnaturels", "smooth_limestone"), new BlockItem(SMOOTH_LIMESTONE, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+        Registry.register(Registry.BLOCK, new Identifier("biomeslesnaturels", "limestone_bricks"), SMOOTH_LIMESTONE_BRICKS);
+        Registry.register(Registry.ITEM, new Identifier("biomeslesnaturels", "limestone_bricks"), new BlockItem(SMOOTH_LIMESTONE_BRICKS, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+        Registry.register(Registry.BLOCK, new Identifier("biomeslesnaturels", "polished_ruffrock"), POLISHED_RUFFROCK);
+        Registry.register(Registry.ITEM, new Identifier("biomeslesnaturels", "polished_ruffrock"), new BlockItem(POLISHED_RUFFROCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+        Registry.register(Registry.BLOCK, new Identifier("biomeslesnaturels", "ruffrock_tiles"), POLISHED_RUFFROCK_TILES);
+        Registry.register(Registry.ITEM, new Identifier("biomeslesnaturels", "ruffrock_tiles"), new BlockItem(POLISHED_RUFFROCK_TILES, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
     }
 }
