6c4bd3fda06a7d2110110471a5a1d95e8c8c8d80
KaloyanKKS <k.yunchov9@gmail.com>
2021-05-02 13:07:16 +0100

    Alcoholism Done

--- src/main/java/me/kaloyankys/biomeslesnaturels/block/JollyBarrel.java
+++ src/main/java/me/kaloyankys/biomeslesnaturels/block/JollyBarrel.java
@@ -7,9 +7,8 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.state.StateManager;
-import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.IntProperty;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -17,51 +16,48 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class JollyBarrel extends Block {
-    public static final DirectionProperty FACING;
-    public static final IntProperty WINE_LEVEL = IntProperty.of("wine_level", 0, 1000);
+    public static final IntProperty WINE_LEVEL = IntProperty.of("wine_level", 0, 5);
+    public static final BooleanProperty OPEN = BooleanProperty.of("open");
+    int i = 0;
 
     public JollyBarrel(Settings settings) {
         super(settings);
+        this.setDefaultState((BlockState) ((BlockState) this.stateManager.getDefaultState()).with(WINE_LEVEL, 0).with(OPEN, false));
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
-        stateManager.add(WINE_LEVEL, FACING);
+        stateManager.add(WINE_LEVEL, OPEN);
     }
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        int i = 0;
         ItemStack itemStack = player.getStackInHand(hand);
-        if (itemStack.getItem() == ModItems.GRAPES) {
-            if (i < 5)
-                if (!world.isClient) {
-                    itemStack.decrement(1);
-                    i++;
-                    world.setBlockState(pos, state.with(WINE_LEVEL, i));
-                    return ActionResult.SUCCESS;
+        if (itemStack.getItem() == ModItems.GRAPES && i < 5) {
+            if (!world.isClient) {
+                itemStack.decrement(1);
+                i++;
+                world.setBlockState(pos, state.with(WINE_LEVEL, i).with(OPEN, true));
+                return ActionResult.SUCCESS;
+            }
+        } if (itemStack.getItem() == Items.GLASS_BOTTLE && i >= 1) {
+            if (!world.isClient) {
+                if (itemStack.isEmpty()) {
+                    player.setStackInHand(hand, new ItemStack(ModItems.WINE_BOTTLE));
+                } else if (!player.inventory.insertStack(new ItemStack(ModItems.WINE_BOTTLE))) {
+                    player.dropItem(new ItemStack(ModItems.WINE_BOTTLE), false);
                 }
-        } else if (itemStack.getItem() == Items.GLASS_BOTTLE) {
-            if (i >= 1)
-                if (!world.isClient) {
-                    if (itemStack.isEmpty()) {
-                        player.setStackInHand(hand, new ItemStack(Items.HONEY_BOTTLE));
-                    } else if (!player.inventory.insertStack(new ItemStack(Items.HONEY_BOTTLE))) {
-                        player.dropItem(new ItemStack(Items.HONEY_BOTTLE), false);
-                    }
-                    itemStack.decrement(1);
-                    i--;
-                    world.setBlockState(pos, state.with(WINE_LEVEL, i));
-                    return ActionResult.SUCCESS;
-                }
-        } else {
-            return super.onUse(state, world, pos, player, hand, hit);
+                itemStack.decrement(1);
+                i--;
+                world.setBlockState(pos, state.with(WINE_LEVEL, i).with(OPEN, true));
+                return ActionResult.SUCCESS;
+            }
+        } else if (i == 0) {
+            world.setBlockState(pos, state.with(OPEN, false));
+        } else if (itemStack.getItem() != ModItems.GRAPES && itemStack.getItem() != Items.GLASS_BOTTLE) {
+            world.setBlockState(pos, state.with(OPEN, false));
         }
-        return ActionResult.SUCCESS;
-    }
-
-    static {
-        FACING = Properties.FACING;
+        return super.onUse(state, world, pos, player, hand, hit);
     }
 }
 
